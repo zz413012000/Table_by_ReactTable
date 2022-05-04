70a5b7d479795809337813b412ac38f1800d0e82
zz413012000 <zz413012000@gmail.com>
2022-05-04 15:56:11 +0800

    CSS

--- src/App.js
+++ src/App.js
@@ -11,7 +11,11 @@ function App() {
       <div className='main'>
         <div className='description'>
           使用技術 React Hook、React Table、fetch<br/>
-          
+          1.fetch 政府開放平台的 api<br/>
+          2.建立 CORS Proxy 部屬在 Heroku<br/>
+          3.使用 useEffect 更新資料<br/>
+          4.React Table 建立表格，提供 sort 功能<br/>
+          5.撰寫 CSS，添加樣式
         </div>
         <Table/>
       </div>
